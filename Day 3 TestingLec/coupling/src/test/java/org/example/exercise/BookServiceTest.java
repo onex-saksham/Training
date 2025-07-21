59bfad6fe95e6cb63547a472c1374f03be9b5e9b
Saksham Jain <saksham.jain@onextel.com>
2025-07-21 10:46:48 +0530

    Day 3+ 4- testing and logging done

---
+++ Day 3 TestingLec/coupling/src/test/java/org/example/exercise/BookServiceTest.java
@@ -0,0 +1,19 @@
+package org.example.exercise;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class BookServiceTest {
+
+    @Test
+    void borrowBook() {
+    }
+
+    @Test
+    void returnBook() {
+    }
+
+    @Test
+    void findAvailableBooksByAuthor() {
+    }
+}
---
+++ Day 3 TestingLec/coupling/src/test/java/org/example/exercise/BookTest.java
@@ -0,0 +1,27 @@
+package org.example.exercise;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class BookTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getTitle() {
+    }
+
+    @Test
+    void getAuthor() {
+    }
+
+    @Test
+    void isAvailable() {
+    }
+
+    @Test
+    void setAvailable() {
+    }
+}
